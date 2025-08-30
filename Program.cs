29be8528e488c08a651b3ea5b63cfa4cb8782edd
Seneen <sneenbadran@gmail.com>
2025-08-30 19:17:58 +0300

    add test method

--- Program.cs
+++ Program.cs
@@ -2,15 +2,9 @@
 {
     internal class Program
     {
+        static void test() { }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
-            Console.WriteLine("Hi");
-            Console.WriteLine("Hi");
-            Console.WriteLine("Hi");
-            Console.WriteLine("Hi");
-            Console.WriteLine("Hi");
-            Console.WriteLine("Hi");
         }
     }
 }
